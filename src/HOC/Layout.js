271248bcd40a77513fa6d642227427849a265cdb
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-24 15:47:09 -0700

    addded Stripes

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../Components/header_footer/Header';
+import Footer from '../Components/header_footer/Footer';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Layout from './HOC/Layout';
+import Home from './Components/home';
 
 const Routes = props => {
-  return <Layout>Second line</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path='/' />
+      </Switch>
+    </Layout>
+  );
 };
 export default Routes;

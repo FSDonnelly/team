6eec321eda68cd3879c375720c7c486fdc5aca33
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-27 21:26:09 -0700

    importing matches from firebaseMatced into Matches component

--- src/Components/matches/index.js
+++ src/Components/matches/index.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { firebaseMatches } from '../../firebase';
+import { firebaseLooper, reverseArray } from '../ui/misc';
 
 class Matches extends Component {
+  state = {
+    loading: true,
+    matches: [],
+    filterMatches: [],
+    playerFilter: 'All',
+    resultFilter: 'All'
+  };
+
+  componentDidMount() {
+    firebaseMatches.once('value').then(snapshot => {
+      const matches = firebaseLooper(snapshot);
+
+      this.setState({
+        loading: false,
+        matches: reverseArray(matches),
+        filterMatches: reverseArray(matches)
+      });
+    });
+  }
+
   render() {
+    console.log(this.state);
     return <div className='the_matches_container'>The Matches</div>;
   }
 }
